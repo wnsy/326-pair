8918eee472b360e36a3915064ea85169a21e1141
wnsy <ayesha.wnz@gmail.com>
2017-01-19 23:30:41 +1300

    Adds user input scanner, store position and state in hashmap #1

--- 01/Ants.java
+++ 01/Ants.java
@@ -10,7 +10,8 @@ class Ants {
         NORTH, EAST, SOUTH, WEST;
         private static Direction[] val = values();
 
-        /** Credit:
+        /**
+         * Credit:
          * http://stackoverflow.com/questions/17006239/
          * whats-the-best-way-to-implement-next-and-previous-on-an-enum-type/17006263#17006263
          */
@@ -26,55 +27,65 @@ class Ants {
     //data struct
     private int antSteps; //ant will take a certain sequence of steps
 
-    
+    /**
+     * First hashmap for Point & State(s). Char for white/black tiles (states)
+     *
+     * |-----------|----------|
+     * |  Point    |  State   |
+     * |-----------|----------|
+     * |  0,0      |    w     |
+     * |-----------|----------|
+     */
+    static HashMap<Point, Character> locationStateMap = new HashMap<Point, Character>();
+
+    /**
+     * Second hashmap for Point & State(s). Char for white/black tiles (states)
+     *
+     * |-----------|----------|
+     * |   Key     |  Value   |
+     * |-----------|----------|
+     * |           |          |
+     * |-----------|----------|
+     */
+    //TODO: rename and check second hashmap
+    static HashMap<Point, Character> somethingElse = new HashMap<>();
+
+
     public static void main(String[] args) {
-        /**
-         * First hashmap for Point & States. string for white/black tile
-         *
-         * |-----------|----------|
-         * |  Point    |  State   |
-         * |-----------|----------|
-         * |  0,0      |    w     | 
-         * |-----------|----------|
-         *
-         */
-        HashMap<Point, Character> locationPoint = new HashMap<Point, Character>();      
+
         Point antJoe = new Point();
-        char storeState = 'w';
+        char storeState = 'w'; //TODO: get and store state from user input
+        locationStateMap.put(new Point(1, 1), storeState);
 
-        locationPoint.put(antJoe, storeState);
-        locationPoint.put(new Point(1,1), storeState);
+        //TODO: while loop, hasNext() blablabla
+        System.out.println("The story of your average Ant Joe :)");
+        Scanner userInputScanner = new Scanner(System.in);
+        System.out.println("Enter x position: ");
+        antJoe.x = userInputScanner.nextInt();
+        System.out.println("Enter y position: ");
+        antJoe.y = userInputScanner.nextInt();
+        System.out.println("Enter the initial character state: ");
+        storeState = userInputScanner.next().charAt(0); //FIXME: accept a blank space, don't accept number as state?
+        locationStateMap.put(antJoe, storeState);
+        //TODO: check toString method from Point class for formatting
+        System.out.println("Ant Joe is located at: " + "[" + antJoe.x + "," + antJoe.y + "]" + " and his state is: "
+                           + locationStateMap.get(antJoe) + "\n");
 
-        //Iterate over all of the keys: 
-        for (Point key : locationPoint.keySet()) {
+
+        //Iterate over all of the keys:
+        System.out.println("Iterate keys:");
+        for (Point key : locationStateMap.keySet()) {
             System.out.println(key);
         }
-        
-
-        System.out.println("Ant Joe is located at: " + "[" +  antJoe.x + "," + antJoe.y + "]");
-
-
-        // System.out.println("Test user input: "); //user prompt
-        
-        // Scanner userInputScanner = new Scanner(System.in);
-        // while (userInputScanner.hasNext()) {
-        //     // antsLocation. = userInputScanner.nextInt(antsLocation); //Point, x, y
-        //     //TODO: Store x, y from java.awt.Point
-        //     // antsLocation.xy = userInputScanner.nextInt(); // store delete 
-        //     storeState = userInputScanner.nextLine();
-        //     location.put(storeLocation, storeState);
-        // }
-        
-        
 
 
         //TO DELETE
-        //        System.out.println("All directions: ");
-        //        Direction directions[] = Direction.values();
-        //        for (Direction dir : Direction.values()) {
-        //            System.out.println(dir);
-        //        }
-        //        System.out.println();
+//        System.out.println("All directions: ");
+//        Direction directions[] = Direction.values();
+//        for (Direction dir : Direction.values()) {
+//            System.out.println(dir);
+//        }
+//        System.out.println();
 
     }
 }
