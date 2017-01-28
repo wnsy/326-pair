41494de585eaeed3b9909557ef1fd1e3182162cc
Ayesha W <ayesha.wnz@gmail.com>
2017-01-28 17:46:04 +1300

    TODO: debug steps

--- 01/Ants.java
+++ 01/Ants.java
@@ -1,61 +1,40 @@
-// Java 8
-/* TODO: add warnings, expected input type, use etc */
-package ants;
-
 import java.awt.Point;
 import java.util.Scanner;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Ants {
-
-    enum Direction {
-        //NORTH(0), EAST(1), SOUTH(2), WEST(3);
-        NORTH, EAST, SOUTH, WEST;
-        private static Direction[] val = values();
-
-        /**
-         * Credit: http://stackoverflow.com/questions/17006239/
-         * whats-the-best-way-to-implement-next-and-previous-on-an-enum-type/17006263#17006263
-         */
-    }
-
-    //data struct
-    private int antSteps; //ant will take a certain sequence of steps //DOUBLE UP WITH noSteps variable
-
-    /**
-     * Hashmap for Point & State(s). Char for white/black tiles (states)
-     *
-     * |-----------|----------| | Point | State | |-----------|----------| | 0,0
-     * | , | |-----------|----------|
-     */
     static HashMap<Point, Character> locationStateMap = new HashMap<Point, Character>();
     static HashMap<Character, char[]> stateChange = new HashMap<Character, char[]>();
     static HashMap<Character, int[]> directionOut = new HashMap<Character, int[]>();
     private static char blank;
 
-//SHOULD do some documentation or something
+    //SHOULD do some documentation or something
+    //yes I agree
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
         int i = 0;
-
-        char[] tempCharArray = new char[4];
-        int[] tempIntArray = new int[4];
         int noSteps = 0;
         boolean scenarioCompleted = false;
 
         while (scan.hasNextLine()) {
-            // System.out.println("Clearing.. ");
             locationStateMap.clear();
             stateChange.clear();
             directionOut.clear();
             i = 0;
             scenarioCompleted = false;
             while (!scenarioCompleted) {
+                char[] tempCharArray = new char[4];
+                int[] tempIntArray = new int[4];
                 char firstTemp;
                 String dnaLine = scan.nextLine();
                 System.out.println(dnaLine); //print line when see it
                 //System.out.println("length: " + dnaLine.length()); //debug
+                if(dnaLine.isEmpty() || dnaLine.charAt(0) == '#'){
+                    System.out.println(dnaLine);
+                    break;
+                }
                 if (i == 0) {
                     blank = dnaLine.charAt(0);
                 }
@@ -78,21 +57,25 @@ public class Ants {
                     }
                     directionOut.put(firstTemp, tempIntArray);
                     temp = dnaLine.substring(7, 11);
-                    for (int j = 0; j < 4; j++) {
+                    System.out.print(firstTemp + "| ");
+                    for (int j = 0; j < 4; j++){
                         tempCharArray[j] = temp.charAt(j);
+                        System.out.print(tempCharArray[j]);
                     }
+                    System.out.println();
                     stateChange.put(firstTemp, tempCharArray);
+
                 } else if (dnaLine.length() < 11) {
                     //System.out.println("char at 0: " + dnaLine.charAt(0)); //debug
                     noSteps = Integer.parseInt(dnaLine.substring(0, dnaLine.length())); //SHOULD HAVE A CATCH METHOD HERE
                     //System.out.println("NO STEPS: " + noSteps); //debug
                     System.out.println("Let's go ant Joe!");
                     System.out.println(walk(noSteps).toString());
-
                     scenarioCompleted = true;
                 } else {
                     System.out.println("Sorry that input is incorrect");
                     System.out.println("Enter DNA lines in the format x NNNN xxxx\n followed by a number up to 10^9");
+                    scenarioCompleted = true;
                 }
             }
         }
@@ -103,21 +86,39 @@ public class Ants {
         char pointState = blank;
         Point currentPoint = new Point(0, 0);
         char outState = blank;
+        char[] debugChar;
+        int[] directions; //debugs
         for (int i = 0; i < noSteps; i++) {
             System.out.println("incoming direction: " + incomingDirection);
-            System.out.println("@ " + currentPoint.toString() + "noSteps left: " + (noSteps - i));
+            System.out.println("@ " + currentPoint.toString() + "noSteps: " + i);
             if (locationStateMap.get(currentPoint) != null) { //determines current tile state
                 pointState = locationStateMap.get(currentPoint); //DOUBLE LOOK UP, potential inefficiency
             } else {
                 pointState = blank;
                 //System.out.println("new coordinate so space is: " + blank);
             }
-            System.out.println("State: " + pointState);
+            System.out.print("State: " + pointState + " ");
+            debugChar = stateChange.get(pointState);
+
+        /*
+        for(char a : debugChar){
+            System.out.print(a);
+        }
+        System.out.println();
+        */
             outState = stateChange.get(pointState)[incomingDirection]; //determine state change based on incoming
+            //System.out.println("out: " + outState);
             locationStateMap.remove(currentPoint);
             if (outState != blank) { //Doesn't store things that can be generated
                 locationStateMap.put(currentPoint, outState); //change the state of the current point based on incoming
             }
+            //debugs
+
+            directions = directionOut.get(pointState);
+            for(int a: directions){
+                System.out.print(a);
+            }
+            System.out.println(); //debug
             incomingDirection = directionOut.get(pointState)[incomingDirection]; //set incoming direction
 
             //change current position
