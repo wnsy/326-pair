e0687caf9c53f30b67472a584bf93861b6d675c1
Ayesha W <ayesha.wnz@gmail.com>
2017-02-09 15:57:24 +1300

    Needs to test

--- 05/Midpoint.java
+++ 05/Midpoint.java
@@ -3,16 +3,29 @@ import java.lang.Math;
 
 public strictfp class Midpoint {
     public static void main(String[] args) {
-        //symmetric midpoint
-        System.out.println(midpoint(Integer.MAX_VALUE, Integer.MAX_VALUE+1000));
-        System.out.println(midpoint(139, 150));
-        System.out.println(mid(150, 139));
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+//        System.out.println(midpoint(Integer.MAX_VALUE, Integer.MAX_VALUE+1000));
 
-        // x ≤ y then x ≤ midpoint(x, y) ≤ y
-        System.out.println(mid(100, 25));
+        //01.symmetric midpoint. midpoint(x, y) = midpoint(y, x)
+        System.out.println("01.symmetric midpoint: " + midpoint(Integer.MAX_VALUE, Integer.MIN_VALUE));
+        System.out.println("01.symmetric midpoint: " + midpoint(Integer.MIN_VALUE, Integer.MAX_VALUE));
+        System.out.println("01.symmetric midpoint: " + midpoint(Integer.MAX_VALUE, Integer.MAX_VALUE+20000));
+        System.out.println("01.symmetric midpoint: " + midpoint(Integer.MAX_VALUE+20000, Integer.MAX_VALUE));
 
-        // min(x, y) ≤ midpoint(x, y) ≤ max(x, y)
-        System.out.println(Math.min(100, 25));
+//        System.out.println(midpoint(139, 150));
+//        System.out.println(midpoint(150, 139));
+
+        //02. x ≤ y then x ≤ midpoint(x, y) ≤ y
+        int x = Integer.MAX_VALUE;
+        int y = Integer.MAX_VALUE+1000;
+        System.out.println("02. x ≤ y then x ≤ midpoint(x, y) ≤ y " +
+                midpoint(x, y));
+        System.out.println("x: " + Math.min(x, y) + " y: " + Math.min(x, y));
+
+        //03. min(x, y) ≤ midpoint(x, y) ≤ max(x, y)
+//        System.out.println(Math.min(100, 25));
+
     }
 
     private static int midpoint(int x, int y) {
@@ -21,6 +34,7 @@ public strictfp class Midpoint {
         } else {
             return (Math.min(x, y) + Math.max(x, y));
         }
-
     }
+
+
 }
